6baba28f32b0c1d2e95290fa2bdce4828b862132
subhradeepa-itboxss <subhradeepa.itboxss@gmail.com>
2026-07-09 13:33:04 +0530

    Enable settings.local.php include and set private file path

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -612,7 +612,7 @@ $settings['update_free_access'] = FALSE;
  * See https://www.drupal.org/documentation/modules/file for more information
  * about securing private files.
  */
-# $settings['file_private_path'] = '';
+$settings['file_private_path'] = 'sites/default/files/private/';
 
 /**
  * Temporary file path:
@@ -894,6 +894,6 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
+ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+ }
